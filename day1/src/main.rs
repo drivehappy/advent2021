6b8b41b91e77d0ebf6da6b08237f897a0aef34c7
drivehappy <drivehappy@gmail.com>
2021-12-02 21:02:09 -0800

    Day1 Part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn main() {
+fn part1() {
     let f = File::open("input.txt").unwrap();
     let lines = io::BufReader::new(f).lines();
 
@@ -13,7 +13,7 @@ fn main() {
 
     let mut increase_count = 0;
     let mut prev_val: &i32 = &data[0];
-    for (i, v) in data.iter().skip(1).enumerate() {
+    for v in data.iter().skip(1) {
         if v > prev_val {
             increase_count += 1;
         }
@@ -21,4 +21,38 @@ fn main() {
     }
 
     println!("Count: {}", increase_count);
+}
+
+fn part2() {
+    let f = File::open("input.txt").unwrap();
+    let lines = io::BufReader::new(f).lines();
+
+    let mut data: Vec<i32> = Vec::new();
+    for l in lines {
+        let v = l.unwrap().parse::<i32>().unwrap();
+        data.push(v);
+    }
+
+    let mut window_sums: Vec<i32> = Vec::new();
+    let w = data.windows(3);
+
+    for v in w {
+        window_sums.push(v[0] + v[1] + v[2]);
+    }
+
+    let mut increase_count = 0;
+    let mut prev_val: &i32 = &window_sums[0];
+    for v in window_sums.iter().skip(1) {
+        if v > prev_val {
+            increase_count += 1;
+        }
+        prev_val = v;
+    }
+
+    println!("Window Increase Count: {}", increase_count);
+}
+
+fn main() {
+    //part1();
+    part2();
 }
